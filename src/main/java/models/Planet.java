da751d3036fef2c71f8c61fbd8395cf1e348e03f
Codebreakerblack <blancohmariel@gmail.com>
2024-11-25 13:00:59 +0100

    Attributes and methods are added to the planet class that initialize values, return values, calculate the planet's density, and determine whether a planet in the solar system is considered outer

--- src/main/java/models/Planet.java
+++ src/main/java/models/Planet.java
@@ -1,5 +1,67 @@
 package models;
 
-public class Planet {
+import enums.PlanetType;
 
+public class Planet {
+    private String name;
+    private int numberOfSatellites;
+    private double mass;
+    private double volume;
+    private int diameter;
+    private int averageDistanceFromSun;
+    private PlanetType type;
+    private boolean observableWithNakedEye;
+
+    public Planet(String name, int numberOfSatellites, double mass, double volume, int diameter,
+            int averageDistanceFromSun, PlanetType type, boolean observableWithNakedEye) {
+        this.name = name;
+        this.numberOfSatellites = numberOfSatellites;
+        this.mass = mass;
+        this.volume = volume;
+        this.diameter = diameter;
+        this.averageDistanceFromSun = averageDistanceFromSun;
+        this.type = type;
+        this.observableWithNakedEye = observableWithNakedEye;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSatellites() {
+        return numberOfSatellites;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public int getAverageDistanceFromSun() {
+        return averageDistanceFromSun;
+    }
+
+    public PlanetType getType() {
+        return type;
+    }
+
+    public boolean isObservableWithNakedEye() {
+        return observableWithNakedEye;
+    }
+
+    public double calculateDensity() {
+        return mass / volume;
+    }
+
+    public boolean isOuterPlanet() {
+        double distanceAU = averageDistanceFromSun * 1e6 / 149597870.0;
+        return distanceAU > 3.4;
+    }
 }
