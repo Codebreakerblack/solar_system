cb680196663ae522707bc4a1e30ab6ab580b75c5
Codebreakerblack <blancohmariel@gmail.com>
2024-11-25 13:25:23 +0100

    Adds the enum for the planet types and view method of the single system planet messages

--- src/main/java/enums/PlanetType.java
+++ src/main/java/enums/PlanetType.java
@@ -1,5 +1,5 @@
 package enums;
 
 public enum PlanetType {
-
+    GASEOUS, TERRESTRIAL, DWARF
 }
--- src/main/java/views/SystemSolarViews.java
+++ src/main/java/views/SystemSolarViews.java
@@ -1,5 +1,7 @@
 package views;
 
 public class SystemSolarViews {
-
+        public static void showMessage(String message) {
+        System.out.println(message);
+    }
 }
